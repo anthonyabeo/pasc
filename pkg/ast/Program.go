cf7d744e306dce0af9384f210502e99c28c6f9f8
Anthony A. Abeo <anthonyabeo@gmail.com>
2022-12-02 01:03:51 +0000

    move body of Program to Block type

--- pkg/ast/Program.go
+++ pkg/ast/Program.go
@@ -3,17 +3,7 @@ package ast
 // ProgramAST defines the root node of the AST. It correlates to the
 // start symbol in the grammer
 type ProgramAST struct {
-	// label definition part
-	// constant definition part
-	// type definition part
-
-	// variable declaration part
-	Vars []*VarDecl
-
-	// procedure and function declaration part
-
-	// statements
-	Stats []Statement
+	Block *Block
 }
 
 // TokenLiteral returns the text value this node's token.
