6d77539aef00a182ecb18950d5822d786f7a81d1
Anthony A. Abeo <anthonyabeo@gmail.com>
2022-12-07 17:34:56 +0000

    introduce token field to AssignmentStatement

--- pkg/ast/AssignStatement.go
+++ pkg/ast/AssignStatement.go
@@ -1,16 +1,21 @@
 package ast
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/anthonyabeo/pasc/pkg/token"
+)
 
 // AssignStatement is the node used to represent Pascal assignments in the AST
 type AssignStatement struct {
+	Token    token.Token
 	Variable *Identifier
 	Value    Expression
 }
 
 // TokenLiteral returns the text value this node's token.
 func (as *AssignStatement) TokenLiteral() string {
-	return as.Variable.Token.Text
+	return as.Variable.Name
 }
 
 func (as *AssignStatement) statNode() {}
