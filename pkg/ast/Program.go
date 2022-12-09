00d4ab7a2645c9bbab31e65b844a1576ee58b7d6
Anthony A. Abeo <anthonyabeo@gmail.com>
2022-12-09 16:26:50 +0000

    add program name and parameter list fields

--- pkg/ast/Program.go
+++ pkg/ast/Program.go
@@ -5,14 +5,16 @@ import "fmt"
 // ProgramAST defines the root node of the AST. It correlates to the
 // start symbol in the grammer
 type ProgramAST struct {
-	Block *Block
+	Name      *Identifier
+	ParamList []*Identifier
+	Block     *Block
 }
 
 // TokenLiteral returns the text value this node's token.
 func (p *ProgramAST) TokenLiteral() string {
-	return "program"
+	return fmt.Sprintf("program %v(%v)", p.Name.String(), p.ParamList)
 }
 
 func (p *ProgramAST) String() string {
-	return fmt.Sprintf("%v", p.Block)
+	return fmt.Sprintf("program %v(%v)", p.Name.String(), p.ParamList)
 }
