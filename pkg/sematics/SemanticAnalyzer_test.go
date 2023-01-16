a5d690c02fcf0f1d2b0609183e54842b8046a6c1
Anthony A. Abeo <anthonyabeo@gmail.com>
2023-01-16 21:32:28 +0000

    create some semblemce of a semantic analyzer

---
+++ pkg/sematics/SemanticAnalyzer_test.go
@@ -0,0 +1,69 @@
+package sematics
+
+import (
+	"testing"
+
+	"github.com/anthonyabeo/pasc/pkg/parser"
+)
+
+func TestStaticTypeCheckAssignmentStatement(t *testing.T) {
+	input := `
+	program HelloWorld;
+	var
+		a, b, sum : integer;
+
+	begin
+		a := 1;
+		b := 2;
+
+		writeln('Hello, world!')
+	end.
+`
+	lex := parser.NewLexer(input)
+	pars, err := parser.NewParser(lex)
+	if err != nil {
+		t.Error(err)
+	}
+
+	prog, err := pars.Program()
+	if err != nil || prog == nil {
+		t.Error(err)
+	}
+
+	semAnal := &SemanticAnalyzer{ast: prog, SymbolTable: pars.SymbolTable()}
+	if err := semAnal.Run(); err != nil {
+		t.Error(err)
+	}
+}
+
+func TestStaticCheckIfStatement(t *testing.T) {
+	input := `
+	program HelloWorld;
+	var
+		n, m, result : integer;
+
+	begin
+		if (n > m) then
+			result := n
+		else
+			result := m;
+
+		writeln('Hello, world!')
+	end.
+`
+	lex := parser.NewLexer(input)
+	pars, err := parser.NewParser(lex)
+	if err != nil {
+		t.Error(err)
+	}
+
+	prog, err := pars.Program()
+	if err != nil || prog == nil {
+		t.Error(err)
+	}
+
+	semAnal := &SemanticAnalyzer{ast: prog, SymbolTable: pars.SymbolTable()}
+	if err := semAnal.Run(); err != nil {
+		t.Error(err)
+	}
+}
