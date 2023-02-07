690a21ea24b65942c98609c0307ed959c9a755a0
Anthony A. Abeo <anthonyabeo@gmail.com>
2023-02-07 20:05:06 +0000

    define boolean data type

---
+++ pkg/types/Boolean.go
@@ -0,0 +1,15 @@
+package types
+
+// Boolean is an integer data type
+type Boolean struct {
+	Name string
+}
+
+// GetName ...
+func (b *Boolean) GetName() string {
+	return b.Name
+}
+
+func (b *Boolean) isBuiltIn() bool {
+	return true
+}
