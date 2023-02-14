c5c3a585d0593eb5d471af7a7032f9fac3e73593
Anthony A. Abeo <anthonyabeo@gmail.com>
2023-02-14 21:02:41 +0000

    introduce while statement model

---
+++ pkg/ast/WhileStatement.go
@@ -0,0 +1,29 @@
+package ast
+
+import (
+	"fmt"
+
+	"github.com/anthonyabeo/pasc/pkg/token"
+)
+
+// WhileStatement models the AST node of a While Statement
+type WhileStatement struct {
+	Token    token.Token
+	BoolExpr Expression
+	Body     Statement
+}
+
+// TokenLiteral returns the text value this node's token.
+func (w *WhileStatement) TokenLiteral() string { return w.Token.Text }
+
+// TokenKind returns this node's token's kind
+func (w *WhileStatement) TokenKind() token.Kind { return w.Token.Kind }
+
+// StatNode ...
+func (w *WhileStatement) StatNode() string {
+	return fmt.Sprintf("while(%v) do %v", w.BoolExpr, w.Body)
+}
+
+func (w *WhileStatement) String() string {
+	return fmt.Sprintf("while(%v) do %v", w.BoolExpr, w.Body)
+}
