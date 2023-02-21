7d27b38ea50b66d48f4b3d5cf46414a246c15edf
Anthony Anabila Abeo <anthonyabeo@gmail.com>
2023-02-21 20:01:55 +0000

    define type and symbol for character data type

---
+++ pkg/types/Char.go
@@ -0,0 +1,15 @@
+package types
+
+// Char models a data type for single characters
+type Char struct {
+	Name string
+}
+
+// GetName returns the character
+func (c *Char) GetName() string {
+	return c.Name
+}
+
+func (c *Char) isBuiltIn() bool {
+	return true
+}
