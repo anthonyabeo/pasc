173e9920c9d3f27eb7a6f7d71cb36b2353ea0095
Anthony Anabila Abeo <anthonyabeo@gmail.com>
2023-02-25 23:03:54 +0000

    define record type

---
+++ pkg/types/structured/Record.go
@@ -0,0 +1,57 @@
+package structured
+
+import (
+	"github.com/anthonyabeo/pasc/pkg/ast"
+	"github.com/anthonyabeo/pasc/pkg/token"
+	"github.com/anthonyabeo/pasc/pkg/types"
+)
+
+// Record denoted a record type
+type Record struct {
+	Token     token.Token
+	FieldList []Field
+}
+
+// GetName ...
+func (r *Record) GetName() string {
+	return "record"
+}
+
+// RecordSection ...
+type RecordSection struct {
+	List []*ast.Identifier
+	Type types.Type
+}
+
+// Field ...
+type Field interface {
+	recordField()
+}
+
+// FixedPart ...
+type FixedPart struct {
+	Entry []*RecordSection
+}
+
+func (f *FixedPart) recordField() {}
+
+// VariantPart ...
+type VariantPart struct {
+	Token           token.Token
+	VariantSelector *VariantSelector
+	Variants        []*Variant
+}
+
+func (v *VariantPart) recordField() {}
+
+// Variant ...
+type Variant struct {
+	CaseConstList []ast.Expression
+	FieldList     []Field
+}
+
+// VariantSelector ...
+type VariantSelector struct {
+	TagField *ast.Identifier
+	TagType  types.Type
+}
