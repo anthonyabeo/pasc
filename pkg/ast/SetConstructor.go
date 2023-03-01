8c95d99eb0f15911b2b183c9d62c792d3c64dd10
Anthony Anabila Abeo <anthonyabeo@gmail.com>
2023-03-01 15:59:13 +0000

    create set constructor type

---
+++ pkg/ast/SetConstructor.go
@@ -0,0 +1,43 @@
+package ast
+
+import (
+	"github.com/anthonyabeo/pasc/pkg/token"
+	"github.com/anthonyabeo/pasc/pkg/types"
+)
+
+// SetConstructor ...
+type SetConstructor struct {
+	Token    token.Token
+	Members  []Expression
+	EvalType types.Type
+}
+
+// TokenLiteral returns the text value this node's token.
+func (s *SetConstructor) TokenLiteral() string { return s.Token.Text }
+
+func (s *SetConstructor) exprNode() {}
+
+// TokenKind ...
+func (s *SetConstructor) TokenKind() token.Kind {
+	return s.Token.Kind
+}
+
+// Attr ...
+func (s *SetConstructor) Attr(attr string) any {
+	switch attr {
+	case "type":
+		return s.EvalType
+	default:
+		return ""
+	}
+}
+
+func (s *SetConstructor) String() string {
+	return "nil"
+}
+
+// RValue ...
+func (s *SetConstructor) RValue() Expression { return nil }
+
+// LValue ...
+func (s *SetConstructor) LValue() Expression { return nil }
