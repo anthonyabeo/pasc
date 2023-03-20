e74ae3e3c08d6f7522cfd69a7584db6e12d35afe
Anthony Anabila Abeo <anthonyabeo@gmail.com>
2023-03-20 19:42:43 +0000

    remove commented code

--- pkg/semantics/StaticTypeChecker.go
+++ pkg/semantics/StaticTypeChecker.go
@@ -35,3 +35,46 @@ func (v *StaticTypeCheckVisitor) Visit(node ast.Node) {
 		panic(fmt.Sprintf("Visit: unexpected node type %T", node))
 	}
 }
+
+// TODO complete implementation
+// a) T1 and T2 are the same type.
+// b) T1 is a subrange of T2, or T2 is a subrange of T1, or both T1 and T2 are subranges of the same host-type.
+// c) T1 and T2 are set-types of compatible base-types, and either both T1 and T2 are designated packed or neither T1 nor T2 is designated packed.
+// d) T1 and T2 are string-types with the same number of components.
+func areCompatibleTypes(a, b types.Type) bool {
+	if equals(a, b) {
+		return true
+	}
+
+	return false
+}
+
+// TODO complete implementation
+// a) T1 and T2 are the same type, and that type is permissible as the component-type of a file-type (see 6.4.3.5).
+// b) T1 is the real-type and T2 is the integer-type.
+// c) T1 and T2 are compatible ordinal-types, and the value of type T2 is in the closed interval specified by the type T1.
+// d) T1 and T2 are compatible set-types, and all the members of the value of type T2 are in the closed interval speci ed by the base-type of T1.
+// e) T1 and T2 are compatible string-types.
+func areAssignmentCompatible(src, dest types.Type) bool {
+	if dest.GetName() == "real" && src.GetName() == "integer" {
+		return true
+	}
+
+	if areCompatibleTypes(src, dest) {
+		return true
+	}
+
+	return false
+}
+
+func equals(a, b types.Type) bool {
+	return a.GetName() == b.GetName()
+}
+
+func isSimpleType(t types.Type) bool {
+	return t.GetName() == "integer" ||
+		t.GetName() == "char" ||
+		t.GetName() == "Boolean" ||
+		t.GetName() == "enum" ||
+		t.GetName() == "subrange"
+}
