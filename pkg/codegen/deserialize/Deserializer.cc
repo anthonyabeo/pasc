1909d2f48f43b74e62b0296b80a0cad961254e4c
Anthony Anabila Abeo <anthonyabeo@gmail.com>
2023-04-05 15:53:31 +0000

    implement deserializer

---
+++ pkg/codegen/deserialize/Deserializer.cc
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <fstream>
+#include <string>
+
+#include "program.pb.h"
+
+#include "Deserializer.h"
+#include "Program.h"
+
+Pasc::Program DeserialiseProtobufFile(std::string &filePath) {
+    Pasc::Program program;
+
+    std::fstream fileIn(filePath, std::ios::in | std::ios::binary);
+    if (!fileIn) {
+        throw DeserialiseProtobufException("File not found.");
+    }
+
+    if (!program.ParseFromIstream(&fileIn)) {
+        throw DeserialiseProtobufException("Protobuf not deserialised from file.");
+    }
+
+    return program;
+}
+
+std::unique_ptr<ProgramIR> CreateInternalIRFromProtobuf(const Pasc::Program& program) {
+    return std::unique_ptr<ProgramIR>(new ProgramIR(program));
+}
---
+++ pkg/codegen/deserialize/Deserializer.h
@@ -0,0 +1,29 @@
+#ifndef DESERIALIZE_H
+#define DESERIALIZE_H
+
+#pragma once
+#include <stdlib.h>
+
+#include <fstream>
+#include <string>
+
+#include "program.pb.h"
+#include "Program.h"
+
+Pasc::Program DeserialiseProtobufFile(std::string &filePath);
+
+std::unique_ptr<ProgramIR> CreateInternalIRFromProtobuf(const Pasc::Program&);
+
+/// @brief DeserialiseProtobufException 
+class DeserialiseProtobufException : public std::exception {
+  std::string errorMessage;
+
+ public:
+  DeserialiseProtobufException(const char msg[]) {
+    std::string errorMessage(msg);
+  }
+  const char *what() const throw() { return errorMessage.c_str(); }
+};
+
+
+#endif // DESERIALIZE_H
