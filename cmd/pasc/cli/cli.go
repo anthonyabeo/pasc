d29c08b5333c974f7dbb32c38c74a51272c72d7e
Anthony A. Abeo <anthonyabeo@gmail.com>
2023-04-23 20:58:07 +0000

    implement Run function

--- cmd/pasc/cli/cli.go
+++ cmd/pasc/cli/cli.go
@@ -1,6 +1,47 @@
 package cli
 
+import (
+	"fmt"
+	"io/ioutil"
+
+	serde "github.com/anthonyabeo/pasc/pkg/codegen/serializer"
+	"github.com/anthonyabeo/pasc/pkg/parser"
+	"github.com/anthonyabeo/pasc/pkg/semantics"
+)
+
 // Run ...
 func Run(args []string) error {
+	if len(args) < 1 {
+		return fmt.Errorf("program file missing")
+	}
+
+	input, err := ioutil.ReadFile(args[0])
+	if err != nil {
+		return err
+	}
+
+	lex := parser.NewLexer(string(input))
+	pars, err := parser.NewParser(lex)
+	if err != nil {
+		return err
+	}
+
+	prog, err := pars.Program()
+	if err != nil {
+		return err
+	}
+
+	sema := &semantics.SemanticAnalyzer{
+		Ast:               prog,
+		ExprEval:          &semantics.ExprEvalVisitor{SymbolTable: pars.SymbolTable()},
+		StaticTypeChecker: &semantics.StaticTypeCheckVisitor{},
+	}
+	sema.Run()
+
+	err = serde.Serialize(serde.AstToProtoAst(*sema.Ast))
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
