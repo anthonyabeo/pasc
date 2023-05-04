9e1b6b29f9dfbe7f98b87dc6561cdb94d479f2fe
Anthony Anabila Abeo <anthonyabeo@gmail.com>
2023-05-04 16:44:40 +0000

    add while program example

---
+++ examples/while.pas
@@ -0,0 +1,16 @@
+program WhileProgram;
+var
+    a, b, sum : integer;
+
+begin
+    sum := 0;
+    a := 5;
+
+    while a >= 0 do
+    begin
+        sum := sum + a;
+        a := a - 1
+    end;
+
+    writeln(sum)
+end.
