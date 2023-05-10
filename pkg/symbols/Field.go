0efd71f05a405432958b6ee8d44856e9f2871b02
Anthony Anabila Abeo <anthonyabeo@gmail.com>
2023-05-10 15:21:56 +0000

    define symbol model for field

---
+++ pkg/symbols/Field.go
@@ -0,0 +1,34 @@
+package symbols
+
+import "github.com/anthonyabeo/pasc/pkg/types"
+
+// Field denotes a record field symbol
+type Field struct {
+	Name string
+	Kind Kind
+	Type types.Type
+}
+
+// NewField creates and returns a new field symbol
+func NewField(name string, kind Kind, typ types.Type) *Field {
+	return &Field{Name: name, Kind: kind, Type: typ}
+}
+
+// GetKind returns the kind of this symbol
+func (f *Field) GetKind() Kind {
+	return f.Kind
+}
+
+// GetName returns the name of this symbol
+func (f *Field) GetName() string {
+	return f.Name
+}
+
+// GetType returns the type of this symbol
+func (f *Field) GetType() types.Type {
+	return f.Type
+}
+
+func (f *Field) String() string {
+	return f.Name
+}
