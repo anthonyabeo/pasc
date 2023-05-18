6a7ffd1f5d531594e9bd45aae50037616bc031ec
Anthony A. Abeo <anthonyabeo@gmail.com>
2023-05-18 20:05:35 +0000

    add for-statement example

---
+++ examples/for-stmt.pas
@@ -0,0 +1,19 @@
+program ForStmt;
+var
+    i, sum : integer;
+    j, res : integer;
+
+begin
+    sum := 0;
+
+    for i := 1 to 5 do
+        sum := sum + i;
+
+    writeln( sum );
+
+    res := 0;
+    for j := 5 downto 1 do
+        res := res + j;
+
+    writeln( res )
+end.
