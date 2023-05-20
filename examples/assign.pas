0b76eb9fc89606fa08a04a98f01474b7acfa25bf
Anthony Anabila Abeo <anthonyabeo@gmail.com>
2023-05-20 17:21:00 +0000

    update examples/assign.pas with real data types

--- examples/assign.pas
+++ examples/assign.pas
@@ -1,9 +1,15 @@
 program Assign;
 var
 	a, b : integer;
+    x, y : real;
 
 begin
 	a := 5;
     b := a;
-	writeln( b )
+	writeln( b );
+
+	x := 1e10;
+	y := 5e-3;
+	writeln( x );
+	writeln( y )
 end.
