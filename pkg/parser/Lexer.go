79c676b46eea79c893d929b96cdcd89f11f3e4f7
Anthony A. Abeo <anthonyabeo@gmail.com>
2023-08-05 17:22:46 +0000

    refactor lexer.NextToken
    This change will prioritize scanning the common case of identifiers,
    keywords and numbers, instead of Operator and other symbols such as
    +, [

--- pkg/parser/Lexer.go
+++ pkg/parser/Lexer.go
@@ -76,103 +76,103 @@ func (lex *Lexer) NextToken() (token.Token, error) {
 	pos := lex.file.Position(lex.file.Pos(lex.offset))
 
 	for lex.ch != eof {
-		switch lex.ch {
-		case '(':
-			lex.consume()
-			return token.Token{Kind: token.LParen, Text: "(", Pos: &pos}, nil
-		case ')':
-			lex.consume()
-			return token.Token{Kind: token.RParen, Text: ")", Pos: &pos}, nil
-		case ';':
-			lex.consume()
-			return token.Token{Kind: token.SemiColon, Text: ";", Pos: &pos}, nil
-		case '\'':
-			lex.consume()
-			tok := token.NewToken(token.StrLiteral, lex.readStringLiteral(), &pos)
-			lex.consume()
+		switch ch := lex.ch; {
+		case lex.isLetter(ch):
+			name := lex.readName()
 
-			return tok, nil
-		case '.':
-			lex.consume()
-			if lex.ch == '.' {
-				lex.consume()
-				return token.Token{Kind: token.Range, Text: "..", Pos: &pos}, nil
+			return token.Token{Kind: token.Lookup(name), Text: name, Pos: &pos}, nil
+		case lex.isDigit(ch):
+			uNum := lex.readUnsignedNumber()
+			if strings.Contains(uNum, "e") || strings.Contains(uNum, ".") {
+				return token.Token{Kind: token.URealLiteral, Text: uNum, Pos: &pos}, nil
 			}
 
-			return token.Token{Kind: token.Period, Text: ".", Pos: &pos}, nil
-		case '[':
-			lex.consume()
-			return token.Token{Kind: token.LSqBrace, Text: "[", Pos: &pos}, nil
-		case ']':
-			lex.consume()
-			return token.Token{Kind: token.RSqBrace, Text: "]", Pos: &pos}, nil
-		case '+':
-			lex.consume()
-			return token.Token{Kind: token.Plus, Text: "+", Pos: &pos}, nil
-		case '-':
-			lex.consume()
-			return token.Token{Kind: token.Minus, Text: "-", Pos: &pos}, nil
-		case '*':
-			lex.consume()
-			return token.Token{Kind: token.Star, Text: "*", Pos: &pos}, nil
-		case '/':
-			lex.consume()
-			return token.Token{Kind: token.FwdSlash, Text: "/", Pos: &pos}, nil
-		case '=':
-			lex.consume()
-			return token.Token{Kind: token.Equal, Text: "=", Pos: &pos}, nil
-		case '<':
-			lex.consume()
-			if lex.ch == '>' {
-				lex.consume()
-				return token.Token{Kind: token.NotEqual, Text: "<>", Pos: &pos}, nil
-			}
-
-			if lex.ch == '=' {
-				lex.consume()
-				return token.Token{Kind: token.LessThanOrEqual, Text: "<=", Pos: &pos}, nil
-			}
-
-			return token.Token{Kind: token.LessThan, Text: "<", Pos: &pos}, nil
-		case '>':
-			lex.consume()
-			if lex.ch == '=' {
-				lex.consume()
-				return token.Token{Kind: token.GreaterThanOrEqual, Text: ">=", Pos: &pos}, nil
-			}
-
-			return token.Token{Kind: token.GreaterThan, Text: ">", Pos: &pos}, nil
-		case ',':
-			lex.consume()
-			return token.Token{Kind: token.Comma, Text: ",", Pos: &pos}, nil
-		case '^':
-			lex.consume()
-			return token.Token{Kind: token.Caret, Text: "^", Pos: &pos}, nil
-		case ':':
-			lex.consume()
-			if lex.ch == '=' {
-				lex.consume()
-				return token.Token{Kind: token.Initialize, Text: ":=", Pos: &pos}, nil
-			}
-
-			return token.Token{Kind: token.Colon, Text: ":", Pos: &pos}, nil
+			return token.Token{Kind: token.UIntLiteral, Text: uNum, Pos: &pos}, nil
 		default:
-			if lex.isLetter() {
-				name := lex.readName()
-				return token.Token{Kind: token.Lookup(name), Text: name, Pos: &pos}, nil
-			}
+			switch lex.ch {
+			case '(':
+				lex.consume()
+				return token.Token{Kind: token.LParen, Text: "(", Pos: &pos}, nil
+			case ')':
+				lex.consume()
+				return token.Token{Kind: token.RParen, Text: ")", Pos: &pos}, nil
+			case ';':
+				lex.consume()
+				return token.Token{Kind: token.SemiColon, Text: ";", Pos: &pos}, nil
+			case '\'':
+				lex.consume()
+				tok := token.NewToken(token.StrLiteral, lex.readStringLiteral(), &pos)
+				lex.consume()
 
-			if lex.isDigit() {
-				uNum := lex.readUnsignedNumber()
-				if strings.Contains(uNum, "e") || strings.Contains(uNum, ".") {
-					return token.Token{Kind: token.URealLiteral, Text: uNum, Pos: &pos}, nil
+				return tok, nil
+			case '.':
+				lex.consume()
+				if lex.ch == '.' {
+					lex.consume()
+					return token.Token{Kind: token.Range, Text: "..", Pos: &pos}, nil
 				}
 
-				return token.Token{Kind: token.UIntLiteral, Text: uNum, Pos: &pos}, nil
-			}
+				return token.Token{Kind: token.Period, Text: ".", Pos: &pos}, nil
+			case '[':
+				lex.consume()
+				return token.Token{Kind: token.LSqBrace, Text: "[", Pos: &pos}, nil
+			case ']':
+				lex.consume()
+				return token.Token{Kind: token.RSqBrace, Text: "]", Pos: &pos}, nil
+			case '+':
+				lex.consume()
+				return token.Token{Kind: token.Plus, Text: "+", Pos: &pos}, nil
+			case '-':
+				lex.consume()
+				return token.Token{Kind: token.Minus, Text: "-", Pos: &pos}, nil
+			case '*':
+				lex.consume()
+				return token.Token{Kind: token.Star, Text: "*", Pos: &pos}, nil
+			case '/':
+				lex.consume()
+				return token.Token{Kind: token.FwdSlash, Text: "/", Pos: &pos}, nil
+			case '=':
+				lex.consume()
+				return token.Token{Kind: token.Equal, Text: "=", Pos: &pos}, nil
+			case '<':
+				lex.consume()
+				if lex.ch == '>' {
+					lex.consume()
+					return token.Token{Kind: token.NotEqual, Text: "<>", Pos: &pos}, nil
+				}
 
-			return token.Token{Kind: token.Illegal, Text: string(lex.ch)},
-				fmt.Errorf("invalid character: %v at %v", string(lex.ch), pos)
+				if lex.ch == '=' {
+					lex.consume()
+					return token.Token{Kind: token.LessThanOrEqual, Text: "<=", Pos: &pos}, nil
+				}
+
+				return token.Token{Kind: token.LessThan, Text: "<", Pos: &pos}, nil
+			case '>':
+				lex.consume()
+				if lex.ch == '=' {
+					lex.consume()
+					return token.Token{Kind: token.GreaterThanOrEqual, Text: ">=", Pos: &pos}, nil
+				}
+
+				return token.Token{Kind: token.GreaterThan, Text: ">", Pos: &pos}, nil
+			case ',':
+				lex.consume()
+				return token.Token{Kind: token.Comma, Text: ",", Pos: &pos}, nil
+			case '^':
+				lex.consume()
+				return token.Token{Kind: token.Caret, Text: "^", Pos: &pos}, nil
+			case ':':
+				lex.consume()
+				if lex.ch == '=' {
+					lex.consume()
+					return token.Token{Kind: token.Initialize, Text: ":=", Pos: &pos}, nil
+				}
+
+				return token.Token{Kind: token.Colon, Text: ":", Pos: &pos}, nil
+			default:
+				return token.Token{Kind: token.Illegal, Text: string(lex.ch)},
+					fmt.Errorf("invalid character: %v at %v", string(lex.ch), pos)
+			}
 		}
 	}
 
@@ -181,7 +181,7 @@ func (lex *Lexer) NextToken() (token.Token, error) {
 
 func (lex *Lexer) readName() string {
 	pos := lex.offset
-	for lex.isLetter() || lex.isDigit() {
+	for lex.isLetter(lex.ch) || lex.isDigit(lex.ch) {
 		lex.consume()
 	}
 
@@ -199,7 +199,7 @@ func (lex *Lexer) readStringLiteral() string {
 
 func (lex *Lexer) readIntLiteral() string {
 	pos := lex.offset
-	for lex.isDigit() {
+	for lex.isDigit(lex.ch) {
 		lex.consume()
 	}
 
@@ -208,7 +208,7 @@ func (lex *Lexer) readIntLiteral() string {
 
 func (lex *Lexer) readUnsignedNumber() string {
 	pos := lex.offset
-	for lex.isDigit() {
+	for lex.isDigit(lex.ch) {
 		lex.consume()
 	}
 
@@ -221,7 +221,7 @@ func (lex *Lexer) readUnsignedNumber() string {
 		lex.consume()
 
 		// fractional-part
-		for lex.isDigit() {
+		for lex.isDigit(lex.ch) {
 			lex.consume()
 		}
 
@@ -233,7 +233,7 @@ func (lex *Lexer) readUnsignedNumber() string {
 			lex.consume()
 		}
 
-		for lex.isDigit() {
+		for lex.isDigit(lex.ch) {
 			lex.consume()
 		}
 	}
@@ -246,20 +246,20 @@ func (lex *Lexer) readUnsignedNumber() string {
 		lex.consume()
 	}
 
-	for lex.isDigit() {
+	for lex.isDigit(lex.ch) {
 		lex.consume()
 	}
 
 	return string(lex.src[pos:lex.offset])
 }
 
-func (lex *Lexer) isDigit() bool {
-	return '0' <= lex.ch && lex.ch <= '9'
+func (lex *Lexer) isDigit(ch rune) bool {
+	return '0' <= ch && ch <= '9'
 }
 
-func (lex *Lexer) isLetter() bool {
-	return 'a' <= lex.ch && lex.ch <= 'z' ||
-		'A' <= lex.ch && lex.ch <= 'Z'
+func (lex *Lexer) isLetter(ch rune) bool {
+	return 'a' <= ch && ch <= 'z' ||
+		'A' <= ch && ch <= 'Z'
 }
 
 func (lex *Lexer) consumeWhiteSpace() {
